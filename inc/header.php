31d59b306bef0ef21fc260313952a422d4ba6dce
James Kraai <james@jameskraai.com>
2014-07-27 16:47:46 -0500

    Add inc/header.php Include into pages 	- index.php 	- contact.php Change link to contact.php in main navigation

---
+++ inc/header.php
@@ -0,0 +1,26 @@
+<html>
+<head>
+	<title><?php echo "Shirts 4 Mike"; ?></title>
+	<link rel="stylesheet" href="css/style.css" type="text/css">
+	<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Oswald:400,700" type="text/css">
+	<link rel="shortcut icon" href="favicon.ico">
+</head>
+<body>
+
+	<div class="header">
+
+		<div class="wrapper">
+
+			<h1 class="branding-title"><a href="./">Shirts 4 Mike</a></h1>
+
+			<ul class="nav">
+				<li class="shirts"><a href="#">Shirts</a></li>
+				<li class="contact"><a href="contact.php">Contact</a></li>
+				<li class="cart"><a href="#">Shopping Cart</a></li>
+			</ul>
+
+		</div>
+
+	</div>
+
+	<div id="content">
--- index.php
+++ index.php
@@ -1,29 +1,4 @@
-<html>
-<head>
-	<title><?php echo "Shirts 4 Mike"; ?></title>
-	<link rel="stylesheet" href="css/style.css" type="text/css">
-	<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Oswald:400,700" type="text/css">
-	<link rel="shortcut icon" href="favicon.ico">
-</head>
-<body>
-
-	<div class="header">
-
-		<div class="wrapper">
-
-			<h1 class="branding-title"><a href="./">Shirts 4 Mike</a></h1>
-
-			<ul class="nav">
-				<li class="shirts"><a href="#">Shirts</a></li>
-				<li class="contact"><a href="#">Contact</a></li>
-				<li class="cart"><a href="#">Shopping Cart</a></li>
-			</ul>
-
-		</div>
-
-	</div>
-
-	<div id="content">
+<?php include('inc/header.php'); ?>
 
 		<div class="section banner">
 
