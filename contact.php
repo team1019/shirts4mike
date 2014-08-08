6a6b06ca42d351cba36f85d75c573d3abf87b610
James Kraai <james@jameskraai.com>
2014-08-08 12:02:45 -0500

    Move contact-process.php code to contact.php 	- execute process code if request_method == POST 	- upon success header redirects to contact-thanks.php

--- contact.php
+++ contact.php
@@ -1,4 +1,19 @@
 <?php
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+	// From contact-process.php
+	$name = $_POST["name"];
+	$email = $_POST["email"];
+	$message = $_POST["message"];
+	$email_body = "";
+	$email_body += "Name: " . $name. "\n";
+	$email_body += "Email: " . $email . "\n";
+	$email_body += "Message: " . $message;
+	// TODO: Send email
+	header("Location: contact-thanks.php");
+	exit;
+}
+
 $pageTitle = "Contact Mike";
 $section = "contact";
 include('inc/header.php');
@@ -7,7 +22,7 @@ include('inc/header.php');
 	<div class="wrapper">
 		<h1>Contact</h1>
 		<p>I&rsquo;d love to hear from you! Complete the form to send me an email.</p>
-		<form method="post" action="contact-process.php">
+		<form method="post" action="contact.php">
 			<table>
 				<tr>
 					<th><label for="name">Name</label></th>
