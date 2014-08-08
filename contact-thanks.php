4cc326f2c9190ce35a05ca8f7a8ebbf97277ac1b
James Kraai <james@jameskraai.com>
2014-08-08 11:46:43 -0500

    Add contact-process.php and contact-thanks.php 	- contact process loads all form variables into PHP variables then redirects to contact-thanks.php 	- contact thanks is a thank you page once the user hits the submit button

---
+++ contact-thanks.php
@@ -0,0 +1,15 @@
+<?php 
+$pageTitle = "Contact Mike";
+$section = "contact";
+include ("inc/header.php");
+?>
+
+	<div class="section page">
+		<div class="wrapper">
+			<h1>Contact</h1>
+			<p>Thanks for contacting mike!</p>
+		</div>
+	</div>
+<?php 
+	include("inc/footer.php");
+ ?>
