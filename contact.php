d8780df1beba46d58608d50ec0f57f18b31b66e1
James Kraai <james@jameskraai.com>
2014-08-09 15:46:49 -0500

    Add basic form validation to our contact form on contact.php 	- check if fields are blank 	- check for email header injections 	- check for spam bots via honey pot method

--- contact.php
+++ contact.php
@@ -1,9 +1,27 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 	// From contact-process.php
-	$name = $_POST["name"];
-	$email = $_POST["email"];
-	$message = $_POST["message"];
+	$name = trim($_POST["name"]);
+	$email = trim($_POST["email"]);
+	$message = trim($_POST["message"]);
+	
+	if ($name == "" OR $email = "" OR $message = "") {
+		echo "You must specify a value for name, email and a message";
+		exit;
+	}
+	// Prevents Email Header Injection Attacks
+	foreach( $_POST as $value ) {
+		if (stripos($value,'content-Type: ') !== FALSE) {
+			echo "There was a problem with the information you entered.";
+			exit;
+		}
+	}
+
+	if ($_POST["address"] != "") {
+		echo "Your form submission has an error.";
+		exit;
+	}
+
 	$email_body = "";
 	$email_body += "Name: " . $name. "\n";
 	$email_body += "Email: " . $email . "\n";
@@ -12,7 +30,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 	header("Location: contact.php?status=thanks");
 	exit;
 }
-
 $pageTitle = "Contact Mike";
 $section = "contact";
 include('inc/header.php');
@@ -21,9 +38,8 @@ include('inc/header.php');
 	<div class="wrapper">
 		<h1>Contact</h1>
 		<?php if (isset($_GET["status"]) AND $_GET["status"] == "thanks") { ?>
-			<p>Thanks for contacting mike!</p>
+		<p>Thanks for contacting mike!</p>
 		<?php } else { ?>
-
 		<p>I&rsquo;d love to hear from you! Complete the form to send me an email.</p>
 		<form method="post" action="contact.php">
 			<table>
@@ -38,6 +54,10 @@ include('inc/header.php');
 				<tr>
 					<th><label for="message">Message</label></th>
 					<td><textarea name="message" id="message"></textarea></td>
+				</tr>
+				<tr style="display: none;">
+					<th><label for="address">Address</label></th>
+					<td><textarea name="address" id="address"></textarea></td>
 				</tr>
 			</table>
 			<input type="submit" value="Send">
