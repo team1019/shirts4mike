7386423abac3a5d1b7549325c98babf9c154bb91
James Kraai <james@jameskraai.com>
2014-08-11 11:10:11 -0500

    Add reciept.php - return paypal page after transaction is complete

---
+++ reciept.php
@@ -0,0 +1,12 @@
+<?php 
+	$pageTitle = "Thank you for your order!";
+	$section = "none;";
+	include("inc/header.php");
+ ?>
+<div class="section page">
+	<div class="wrapper">
+		<h1>Thank you!</h1>
+		<p>Thank you for your payment, you can view this transaction at your paypal.</p>
+		<p>If you would like to purchase another shirt, return to our <a href="shirts.php">store</a>!</p>
+	</div>
+</div>
